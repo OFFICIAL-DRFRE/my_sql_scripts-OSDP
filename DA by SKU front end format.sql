3341f03d1ad3a8548efb9b03ad2d9c089be6587d
FREHIWOT <100209006+OFFICIAL-DRFRE@users.noreply.github.com>
2023-03-08 22:44:49 +0300

    Update DA by SKU front end format.sql

--- DA by SKU front end format.sql
+++ DA by SKU front end format.sql
@@ -10,5 +10,5 @@ td.loss_qty1 l_CS,td.Loss_QTY2 l_DZ,td.loss_QTY3 l_PC, td.AMOUNT
 inner join TRANS_DETAIL td on td.DISTRIBUTOR=da.DISTRIBUTOR and da.DOCUMENT=td.DOCUMENT and da.doc_no=td.doc_no
 inner join distributor d on d.DISTRIBUTOR=da.DISTRIBUTOR
 inner join sku s on s.SKU=td.sku  
-where td.DOC_DATE between @datefrom and @dateTo  and da.STATUS=1 
+where td.DOC_DATE between @datefrom and @dateTo  and da.STATUS=1 -- use 0 if you want to see the new DAs
 order by td.DOC_DATE, DOC_NO,td.sku 
