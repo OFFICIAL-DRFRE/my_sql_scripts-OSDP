a2bcc4c3f753e38600f49291e9127eb7ff625d03
OFFICIAL-DRFRE <drfre08@gmail.com>
2023-03-29 12:15:17 +0300

    PK and FK relationship scripts

---
+++ FK relationships.sql
@@ -0,0 +1,22 @@
+SELECT
+    fk.name 'FK Name',
+    tp.name 'Parent table',
+    cp.name, cp.column_id,
+    tr.name 'Refrenced table',
+    cr.name, cr.column_id
+FROM 
+    sys.foreign_keys fk
+INNER JOIN 
+    sys.tables tp ON fk.parent_object_id = tp.object_id
+INNER JOIN 
+    sys.tables tr ON fk.referenced_object_id = tr.object_id
+INNER JOIN 
+    sys.foreign_key_columns fkc ON fkc.constraint_object_id = fk.object_id
+INNER JOIN 
+    sys.columns cp ON fkc.parent_column_id = cp.column_id AND fkc.parent_object_id = cp.object_id
+INNER JOIN 
+    sys.columns cr ON fkc.referenced_column_id = cr.column_id AND fkc.referenced_object_id = cr.object_id
+where 
+	tp.name like '%GRN%'
+ORDER BY
+    tp.name, cp.column_id
