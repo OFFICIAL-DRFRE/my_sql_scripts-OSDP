f72dc449893f23068fb464832b1b604192ed80c5
OFFICIAL-DRFRE <drfre08@gmail.com>
2023-05-07 14:55:58 +0300

    added constraints to pjp temp table

--- section not scheduled.sql
+++ section not scheduled.sql
@@ -2,18 +2,21 @@ use centegy_sndpro_uet
 Declare @sdate date = (select working_date from distributor)
 Declare @weekno varchar(2)=(select week_no from JC_WEEK where START_DATE <= @sdate and END_DATE >= @sdate)
 
-select DISTINCT  @weekno week_no, datename(WEEKDAY, getdate()) Today, PD.PJP, ph.ldesc PJP,pd.section, s.ldesc section, 
+select DISTINCT  @weekno week_no, datename(WEEKDAY, getdate()) Today, PD.PJP, ph.ldesc PJP, ph.ACTIVE,pd.section, s.ldesc section, 
 	case when MON = 1 then N'✓' else ' ' end MON, 
 	case when TUE = 1 then N'✓' else ' ' end TUE, 
 	case when WED = 1 then N'✓' else ' ' end WED, 
 	case when THU = 1 then N'✓' else ' ' end THU, 
 	case when FRI = 1 then N'✓' else ' ' end FRI,
-	case when SAT = 1 then N'✓' else ' ' end SAT
-  --case when SUN = 1 then N'✓' else ' ' end SUN
+	case when SAT = 1 then N'✓' else ' ' end SAT,
+  --case when SUN = 1 then N'✓' else ' ' end SUN,
+  WEEKS_IN_CYCLE 'Weeks/cycle', d.NAME DSR,d.P_WORD password,d.VEHICLE, DEVICE_SERIAL_NO
 from PJP_detail pd 
 inner join PJP_head ph on ph.pjp=pd.pjp and ph.sell_category=pd.sell_category
 inner join section s on s.section = pd.section and s.sell_category =pd.sell_category 
+inner join DSR d on d.DSR=ph.DSR
 where pd.week_no = @weekno and pd.PJP not in ( select pjp from cashmemo where doc_date = @sdate) 
+and ph.PJP = 'A001'
 
 UPDATE PJP_detail SET 	
 	MON = (CASE when datename(weekday, @SDATE) = 'Monday' 	 then 0 ELSE MON END),
--- section pop from temp table.sql
+++ section pop from temp table.sql
@@ -1,32 +1,37 @@
-create table sectionpop_temp(
-town varchar(6), 
-locality varchar(3),
-slocality varchar(3),
-pop varchar(10),
-pjp varchar(4),
-section varchar(5),
-pop_index smallint)
-
-
-alter table section_pop_permanent nocheck constraint FK_SECTION_POP_PERMANENT__POP
-alter table section_pop nocheck constraint FK_SECTION_POP__POP
+CREATE TABLE sectionpop_temp (
+    [COMPANY] [varchar](10) NOT NULL,
+    [DISTRIBUTOR] [varchar](10) NOT NULL,
+    [SELL_CATEGORY] [varchar](3) NOT NULL,
+    TOWN varchar(6), 
+    LOCALITY varchar(3),
+    SLOCALITY varchar(3),
+    POP varchar(10),
+    PJP varchar(4),
+    SECTION varchar(5),
+    POP_INDEX smallint,
+    CONSTRAINT FK_sectionpop_temp_section 
+        FOREIGN KEY (COMPANY, DISTRIBUTOR, SELL_CATEGORY, section)
+        REFERENCES [dbo].[SECTION] (COMPANY, DISTRIBUTOR, SELL_CATEGORY, SECTION),
+    CONSTRAINT FK_sectionpop_temp_pop 
+        FOREIGN KEY (COMPANY, TOWN, LOCALITY, SLOCALITY, POP, DISTRIBUTOR)
+        REFERENCES [dbo].[POP] (COMPANY, TOWN, LOCALITY, SLOCALITY, POP, DISTRIBUTOR)
+);
 go
 
 declare @dtcode varchar(10) = (select top 1 DISTRIBUTOR from DISTRIBUTOR)
 
 insert into SECTION_POP_PERMANENT
-(COMPANY,TOWN,LOCALITY,SLOCALITY,POP,DISTRIBUTOR,SELL_CATEGORY,STATUS_DAY,SECTION,PJP, pop_index)
-select '23',TOWN,LOCALITY,slocality,pop,@dtcode,'001','99',section,pjp, pop_index  from sectionpop_temp
-where town+locality+slocality+pop not in ( select town+locality+slocality+pop from SECTION_POP_PERMANENT)
+	(COMPANY,TOWN,LOCALITY,SLOCALITY,POP,DISTRIBUTOR,SELL_CATEGORY,STATUS_DAY,SECTION,PJP, pop_index)
+select 
+	'23',TOWN,LOCALITY,slocality,pop,@dtcode,'001','99',section,pjp, pop_index  from sectionpop_temp
+where 
+	town+locality+slocality+pop not in ( select town+locality+slocality+pop from SECTION_POP_PERMANENT)
 
 insert into SECTION_POP
-(COMPANY,TOWN,LOCALITY,SLOCALITY,POP,DISTRIBUTOR,SELL_CATEGORY,STATUS_DAY,SECTION,PJP, pop_index)
-select '23',TOWN,LOCALITY,slocality,pop,@dtcode,'001','99',section,pjp, pop_index  from sectionpop_temp
-where town+locality+slocality+pop not in ( select town+locality+slocality+pop from SECTION_POP)
+	(COMPANY,TOWN,LOCALITY,SLOCALITY,POP,DISTRIBUTOR,SELL_CATEGORY,STATUS_DAY,SECTION,PJP, pop_index, working_date)
+select 
+	'23',TOWN,LOCALITY,slocality,pop,@dtcode,'001','99',section,pjp, pop_index , (select working_date from distributor) from sectionpop_temp
+where 
+	town+locality+slocality+pop not in ( select town+locality+slocality+pop from SECTION_POP)
 go
 
-
-update section_pop set working_date = (select working_date from distributor) where working_date is null
-go
-alter table section_pop_permanent check constraint FK_SECTION_POP_PERMANENT__POP
-alter table section_pop check constraint FK_SECTION_POP__POP
